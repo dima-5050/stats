28aed24585d7b943176c966809b12fc34260f355
Sami <dima-5050@mail.ru>
2021-10-10 14:02:40 +0500

    реализован тесты

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module github.com/dima-5050/stats
 
 go 1.17
+
+require github.com/dima-5050/bank v0.0.0-20211010090006-316f9a75daec 
+// indirect
--- pkg/stats/example_test.go
+++ pkg/stats/example_test.go
@@ -1,1 +1,57 @@
 package stats
+
+import (
+	"fmt"
+	"github.com/dima-5050/bank/pkg/bank/types"
+)
+
+func ExampleAvg() {
+	
+	payment := []types.Payment{
+		{
+			ID: 1,
+			Amount: 1_000_00,
+			Category: "Auto",
+		},
+		{
+			ID: 2,
+			Amount: 2_000_00,
+			Category: "Restaurant",
+		},
+		{
+			ID: 3,
+			Amount: 3_000_00,
+			Category: "cafe",
+		},
+	}
+
+	fmt.Println(Avg(payment))
+	//Output: 200000
+	
+}
+
+func ExampleTotalInCategory() {
+	
+	
+	payment := []types.Payment{
+		{
+			ID: 1,
+			Amount: 1_000_00,
+			Category: "Auto",
+		},
+		{
+			ID: 2,
+			Amount: 2_000_00,
+			Category: "Restaurant",
+		},
+		{
+			ID: 3,
+			Amount: 3_000_00,
+			Category: "cafe",
+		},
+	}
+
+	fmt.Println(TotalInCategory(payment,"cafe"))
+	//Output: 300000
+	
+}
