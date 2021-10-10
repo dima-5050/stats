98ba0bb0422c81497cc5d8442c143a6bcbee5fa7
Sami <dima-5050@mail.ru>
2021-10-10 21:34:06 +0500

    realese func CategoriesAvg

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -34,3 +34,16 @@ func TotalInCategory(payments []types.Payment, category types.Category) types.Mo
 	}
 	return total
 }
+
+func CategoriesAvg(payments []types.Payment)map[types.Category]types.Money  {
+	categories:=map[types.Category]types.Money{}
+	// count := 0
+
+	for _, payment := range payments {
+		if payment.Status==types.StatusFail {
+			continue
+		}
+		categories[payment.Category]+=payment.Amount
+	}
+	return categories
+}
